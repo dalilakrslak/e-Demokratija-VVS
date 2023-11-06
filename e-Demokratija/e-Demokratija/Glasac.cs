ab96d67070824848e71b593b15c2c507e01807bf
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-11-06 21:51:39 +0100

    dodana verifikacija glasaca

--- e-Demokratija/e-Demokratija/Glasac.cs
+++ e-Demokratija/e-Demokratija/Glasac.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,8 +16,11 @@ namespace e_Demokratija
 
         public Glasac(String ime, String prezime, DateTime datumRodjenja)
         {
+            DaLiJeImeIspravno(ime);
             this.ime = ime;
+            DaLiJePrezimeIspravno(prezime);
             this.prezime = prezime;
+            DaLiJeDatumaRodjenjaIspravan(datumRodjenja);
             this.datumRodjenja = datumRodjenja;
             FormirajKodGlasaca();
         }
@@ -53,6 +57,81 @@ namespace e_Demokratija
             }
         }
 
+        private void DaLiJeImeIspravno(string ime) 
+        {
+            if (ime.Length == 0)
+                throw new ArgumentException("Ime ne može biti prazna riječ!");
+
+            if (ime == null)
+                throw new ArgumentNullException("Glasač mora imati ime - ime ne smije biti NULL!");
+
+            bool samoCrtice = true;
+            int brojCrtica = 0;
+
+            foreach (char c in ime.ToCharArray())
+            {
+                if (!Char.IsLetter(c))
+                {
+                    if (c == '-')
+                    {
+                        brojCrtica++;
+                        continue;
+                    }
+                    else
+                    {
+                        samoCrtice = false;
+                        break;
+                    }
+                }
+            }
+
+            if (ime.Length < 3 || ime.Length > 20 || !samoCrtice || brojCrtica > 1)
+                throw new ArgumentOutOfRangeException("Upisano ime nije validno!");
+        }
+
+        private void DaLiJePrezimeIspravno(string prezime)
+        {
+            if (ime.Length == 0)
+                throw new ArgumentException("Prezime ne može biti prazna riječ!");
+
+            if (ime == null)
+                throw new ArgumentNullException("Glasač mora imati prezime - prezime ne smije biti NULL!");
+
+            bool samoCrtice = true;
+            int brojCrtica = 0;
+
+            foreach (char c in ime.ToCharArray())
+            {
+                if (!Char.IsLetter(c))
+                {
+                    if (c == '-')
+                    {
+                        brojCrtica++;
+                        continue;
+                    }
+                    else
+                    {
+                        samoCrtice = false;
+                        break;
+                    }
+                }
+            }
+
+            if (ime.Length < 3 || ime.Length > 20 || !samoCrtice || brojCrtica > 1)
+                throw new ArgumentOutOfRangeException("Upisano prezime nije validno!");
+        }
+
+        private void DaLiJeDatumaRodjenjaIspravan(DateTime datum)
+        {
+            if (datum > DateTime.Now)
+                throw new ArgumentOutOfRangeException("Datum rođenja ne može biti veći od današnjeg datuma!");
+
+            int godine = DateTime.Now.Year - datumRodjenja.Year;
+            if (DateTime.Now.Year - datum.Year < 18 || (DateTime.Now.Year - datumRodjenja.Year == 18 && DateTime.Now.Date < datum.AddYears(18).Date))
+            {
+                throw new ArgumentOutOfRangeException("Glasač nije punoljetan!");
+            }
+        }
 
         void FormirajKodGlasaca()
         {
