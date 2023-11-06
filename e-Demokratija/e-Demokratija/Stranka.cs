383b66de5867ea2fb0e2d8f4d106eb54e93fa1b7
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-11-06 23:01:03 +0100

    implementirana klasa Stranka

---
+++ e-Demokratija/e-Demokratija/Stranka.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Demokratija
+{
+    public class Stranka
+    {
+        private string naziv;
+        private string opis;
+        private int brojGlasova;
+        private List<Kandidat> rukovodstvo;
+
+        public Stranka(string naziv, string opis)
+        {
+            this.naziv = naziv;
+            this.opis = opis;
+            brojGlasova = 0;
+            rukovodstvo = new List<Kandidat>();
+        }
+
+        public string Naziv { 
+            get => naziv; 
+            set => naziv = value; 
+        }
+        public string Opis { 
+            get => opis; 
+            set => opis = value; 
+        }
+        public int BrojGlasova { 
+            get => brojGlasova; 
+            set => brojGlasova = value; 
+        }
+        public List<Kandidat> Rukovodstvo { 
+            get => rukovodstvo; 
+            set => rukovodstvo = value; 
+        }
+
+    }
+}
