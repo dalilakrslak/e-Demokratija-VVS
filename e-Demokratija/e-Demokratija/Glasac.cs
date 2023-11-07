9dbd40b1794f19a47dd4dfa0a2cba9b80617ec12
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-11-07 08:20:04 +0100

    promjene u klasi Glasac

--- e-Demokratija/e-Demokratija/Glasac.cs
+++ e-Demokratija/e-Demokratija/Glasac.cs
@@ -31,6 +31,7 @@ namespace e_Demokratija
             get => ime;
             set
             {
+                DaLiJeImeIspravno(ime);
                 ime = value;
             }
         }
@@ -39,6 +40,7 @@ namespace e_Demokratija
             get => prezime;
             set
             {
+                DaLiJePrezimeIspravno(prezime);
                 prezime = value;
             }
         }
@@ -47,6 +49,7 @@ namespace e_Demokratija
             get => datumRodjenja;
             set
             {
+                DaLiJeDatumaRodjenjaIspravan(datumRodjenja);
                 datumRodjenja = value;
             }
         }
