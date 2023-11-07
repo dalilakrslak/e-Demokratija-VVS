db100b79c7f6b2782c78c573e09dd36a969e4662
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-11-07 08:20:16 +0100

    implementirana klasa Kandidat

---
+++ e-Demokratija/e-Demokratija/Kandidat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Demokratija
+{
+    public enum Pozicija {
+        gradonacelnik, 
+        nacelnik, 
+        vijecnik
+    };
+    public class Kandidat: Glasac
+    {
+        private Pozicija pozicija; 
+        private string opisKandidata;
+        private Stranka stranka;
+        public Kandidat() { }
+        public Kandidat(Pozicija pozicija, string opisKandidata, Stranka stranka)
+        {
+            this.pozicija = pozicija;
+            this.opisKandidata = opisKandidata;
+            this.stranka = stranka;
+        }
+
+        public Pozicija Pozicija{ 
+            get => pozicija; 
+            set => pozicija = value; 
+        }
+        public Stranka Stranka { 
+            get => stranka; 
+            set => stranka = value; 
+        }
+        public string OpisKandidata { 
+            get => opisKandidata; 
+            set => opisKandidata = value; 
+        }
+
+
+    }
+}
