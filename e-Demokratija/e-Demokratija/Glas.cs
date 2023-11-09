17d4f48f2aff9043ee30d9a93a734d7279ff0789
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-11-09 10:05:42 +0100

    implementirana klasa Glas

---
+++ e-Demokratija/e-Demokratija/Glas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Demokratija
+{
+    public class Glas
+    {
+        private Glasac glasac;
+        private Kandidat kandidat;
+
+        public Glas(Glasac glasac, Kandidat kandidat, DateTime datumGlasanja)
+        {
+            this.glasac = glasac;
+            this.kandidat = kandidat;
+
+            if (kandidat.Stranka != null && kandidat.Pozicija.Equals(Pozicija.vijecnik))
+            {
+                kandidat.BrojGlasova = kandidat.BrojGlasova + 1;
+                kandidat.Stranka.BrojGlasova = kandidat.Stranka.BrojGlasova + 1;
+            }
+            else
+            {
+                kandidat.BrojGlasova = kandidat.BrojGlasova + 1;
+            }
+
+            if (kandidat.Pozicija == Pozicija.gradonacelnik)
+                glasac.DaLiJeGlasaoZaGradonacelnika = true;
+            if (kandidat.Pozicija == Pozicija.nacelnik)
+                glasac.DaLiJeGlasaoZaNacelnika = true;
+            if (kandidat.Pozicija == Pozicija.vijecnik)
+                glasac.DaLiJeGlasaoZaVijecnika = true;
+        }
+        public Glasac Glasac { 
+            get => glasac; 
+            set => glasac = value; 
+        }
+        public Kandidat Kandidat { 
+            get => kandidat; 
+            set => kandidat = value; 
+        }
+    }
+}
